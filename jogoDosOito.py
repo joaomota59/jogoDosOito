b339466196cec0efc970b159053cf2f8e20e702e
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2020-11-27 23:27:49 -0300

    add função filhosPossiveis

--- jogoDosOito.py
+++ jogoDosOito.py
@@ -1,10 +1,11 @@
 from pptree import *
 import numpy as np
 from tkinter import * #importamos o pacote tkinter
+from copy import deepcopy
 
 ##Funções e Classes##
 
-def encontraPosicao(matrix,value):#retorna a posicao(linha,coluna) de um valor na matriz
+def encontraPosicao(matrix,value = ''):#retorna a posicao(linha,coluna) de um valor na matriz
     return [(index, row.index(value)) for index, row in enumerate(matrix) if value in row][0]
 
 class Arvore:
@@ -19,11 +20,38 @@ class Arvore:
 #k = Arvore(matrix)
 #k.printArvore()
 
-
+def filhosPossiveis(matrix): #retorna todos os filhos possiveis do estado em branco
+    indice = encontraPosicao(matrix) #retorna o indice do espaço em branco na matrix (linha,coluna)
+    NosFilhos = [] #vetor que guarda todos filhos possiveis(matrizes)
+    if(indice[0]-1>=0):#tenta mover o espaço branco p linha anterior
+        a = deepcopy(matrix) #copia a matriz original para a variável w
+        a[indice[0]-1][indice[1]] = ''
+        a[indice[0]][indice[1]] = matrix[indice[0]-1][indice[1]]
+        NosFilhos.append(a)
+    if(indice[0]+1<=2):#tenta mover o espaço branco p linha sucessora
+        b = deepcopy(matrix)
+        b[indice[0]+1][indice[1]] = ''
+        b[indice[0]][indice[1]] = matrix[indice[0]+1][indice[1]]
+        NosFilhos.append(b)
+    if(indice[1]-1>=0):#tenta mover o espaço branco p coluna anterior
+        c = deepcopy(matrix)
+        c[indice[0]][indice[1]-1] = ''
+        c[indice[0]][indice[1]] = matrix[indice[0]][indice[1]-1]
+        NosFilhos.append(c)
+    if(indice[1]+1<=2):#tenta mover o espaço branco p coluna sucessora
+        d = deepcopy(matrix)
+        d[indice[0]][indice[1]+1] = ''
+        d[indice[0]][indice[1]] = matrix[indice[0]][indice[1]+1]
+        NosFilhos.append(d)
+    return NosFilhos
 
 def buscaEmProfundidade(mat):#incompleto
     if(np.array_equal(mat,[['1','2','3'],['4','5','6'],['7','8','']])): #Compara a matriz atual com a matriz de estado final
         return True
+    
+    
+    
+    
 def buscaEmLargura(mat):#incompleto
     return
 def BuscaHeuristica(mat):#incompleto
@@ -34,6 +62,8 @@ def A(mat):#incompleto
 def start():
     matrix = [[entrada.get(),entrada2.get(),entrada3.get()],[entrada4.get(),entrada5.get(),entrada6.get()],[entrada7.get(),entrada8.get(),entrada9.get()]]
     print(matrix)
+    raiz = Arvore(matrix)
+    
     if(str(escolha.get())=='profundidade'):
         buscaEmProfundidade(matrix)
     elif(str(escolha.get())=='largura'):
