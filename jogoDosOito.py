86025fc0b447bb2932b52db2064370d24fdb3c86
João Lucas Mota <joaomota59@hotmail.com>
2020-11-29 00:23:43 -0300

    exibição da árvore de profundidade até certa quantidade de nós na pilha + mudança na biblioteca de exibição da árvore

--- jogoDosOito.py
+++ jogoDosOito.py
@@ -1,24 +1,26 @@
+#pip install PyQt5 + pptree + numpy + tkinter + copy + ete3
 from pptree import *
 import numpy as np
 from tkinter import * #importamos o pacote tkinter
 from copy import deepcopy
+from ete3 import Tree, TreeStyle, Tree, TextFace, add_face_to_node
 
 ##Funções e Classes##
-
-def encontraPosicao(matrix,value = ''):#retorna a posicao(linha,coluna) de um valor na matriz
-    return [(index, row.index(value)) for index, row in enumerate(matrix) if value in row][0]
 
 class Arvore:
     def __init__(self,matrizInicial):#construtor
         self.raiz = Node(matrizInicial)
-    def printArvore(self):# printa a árvore atual
+        self.dicio = {}#guarda tds estados visitados
+        self.dicio[str(matrizInicial)] = self.raiz #guarda a pos na memoria da raiz
+        self.arestas = []
+    def printArvore(self):# printa a árvore atual    
         print_tree(self.raiz, horizontal=False)
-    #def criaAresta(no1,no2)
+    def criaAresta(self,no2,no1):#no2 = nó pai no1= nó filho
+        self.arestas.append((str(no2.name),str(no1)))
+        self.dicio[str(no1)] = Node(str(no1),no2)
 
-
-########
-#k = Arvore(matrix)
-#k.printArvore()
+def encontraPosicao(matrix,value = ''):#retorna a posicao(linha,coluna) de um valor na matriz
+    return [(index, row.index(value)) for index, row in enumerate(matrix) if value in row][0]
 
 def filhosPossiveis(matrix): #retorna todos os filhos possiveis do estado em branco
     indice = encontraPosicao(matrix) #retorna o indice do espaço em branco na matrix (linha,coluna)
@@ -45,10 +47,45 @@ def filhosPossiveis(matrix): #retorna todos os filhos possiveis do estado em bra
         NosFilhos.append(d)
     return NosFilhos
 
+def mostraArvore(arestas=[]):
+    t = Tree.from_parent_child_table(arestas)
+    ts = TreeStyle()
+    ts.show_leaf_name = False
+    def my_layout(node): #coloca o nome em cada nó
+            F = TextFace(node.name, tight_text=True)
+            add_face_to_node(F, node, column=0, position="branch-right")
+            F.rotation = -90#rotação do nome no nó
+        
+    ts.layout_fn = my_layout
+    ts.rotation = 90#exibir árvore na vertical
+    t.show(tree_style=ts)#mostra árvore
+
+
 def buscaEmProfundidade(mat):#incompleto
-    if(np.array_equal(mat,[['1','2','3'],['4','5','6'],['7','8','']])): #Compara a matriz atual com a matriz de estado final
-        return True
-    
+    arvore = Arvore(mat) #Instancia a classe e já cria o nó raiz
+    noNaoVisitado = [mat] #pilha já começa com o nó raiz
+    while(len(noNaoVisitado)>0):
+        pai = noNaoVisitado.pop()#viu que matriz não era solução então tira da pilha
+        mat = pai
+        #for i in pai: #printa o nó em que o algoritmo está no momento
+        #    print(i)
+        #print("\n\n")
+        if(np.array_equal(mat,[['1','2','3'],['4','5','6'],['7','8','']])): #Compara a matriz atual com a matriz de estado final
+            print("ACHOU A SOLUÇÃO")
+            print("Quantidade na Pilha:",len(noNaoVisitado))
+            print(arvore.arestas[0])
+            break
+        for i in filhosPossiveis(mat)[::-1]:#percore os nós filhos de trás para frente
+            try:
+                arvore.dicio[str(i)] #verifica se o filho está no dicionário
+            except:#se não tiver no dicionario(filho visitado) então adiciona ao dicionario
+                arvore.criaAresta(arvore.dicio[str(pai)],i)
+                noNaoVisitado.append(i)#add todos nós filhos
+        if len(noNaoVisitado)==100: #mostra a arvore quando tem 100 nós na pilha
+            print(arvore.arestas[0])
+            mostraArvore(arvore.arestas)
+            break
+        #print(arvore.arestas[0])
     
     
     
@@ -61,9 +98,7 @@ def A(mat):#incompleto
 
 def start():
     matrix = [[entrada.get(),entrada2.get(),entrada3.get()],[entrada4.get(),entrada5.get(),entrada6.get()],[entrada7.get(),entrada8.get(),entrada9.get()]]
-    print(matrix)
-    raiz = Arvore(matrix)
-    
+    #print(matrix)
     if(str(escolha.get())=='profundidade'):
         buscaEmProfundidade(matrix)
     elif(str(escolha.get())=='largura'):
