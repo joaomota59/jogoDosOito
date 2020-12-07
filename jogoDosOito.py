2052cdd9b6bcf876af6d1b99791bf51b663d805f
João Lucas Mota <joaomota59@hotmail.com>
2020-12-07 00:36:39 -0300

    busca a estrela concluída + função de paridade para todas buscas

--- jogoDosOito.py
+++ jogoDosOito.py
@@ -8,14 +8,14 @@ from ete3 import Tree, TreeStyle, Tree, TextFace, add_face_to_node
 import time
 import threading
 from threading import Thread
-import random
+from random import shuffle
 
 ##Funções##
 
 class Arvore: #classe para exibir a arvore
     def __init__(self):
         pass
-    def stringArvore(self,listaFilhos=[],pai="",kw="",tipo='profundidade'):#retorna a árvore em uma string 
+    def stringArvore(self,listaFilhos=[],pai="",kw=""):#retorna a árvore em uma string 
          aux="("#abre para fazer a tupla de filhos
          for i in listaFilhos:
               aux+= str(i).replace("], [","*").replace("[","").replace("]","").replace(",",' ')+"," #colocar estrela pois não dá crto se colocar \n quando passar na instância Tree()
@@ -24,11 +24,7 @@ class Arvore: #classe para exibir a arvore
          pai = pai.replace("], [","*").replace("[","").replace("]","").replace(",",' ')
          if kw=="": #se for o nó raiz e seus filhos
               return aux+pai+";"
-         posPai = -1 #possição do nó pai na durante a busca(valor default - poderia ser qualquer num)
-         if tipo == 'profundidade' or tipo=='heuristica':
-             posPai = kw.index(pai)#procura pelo começo o pai na string
-         elif tipo == 'largura':
-             posPai = kw.index(pai)#procura pelo começo o pai na string 
+         posPai = kw.index(pai) #possição do nó pai na string
          return kw[:posPai] +aux+ kw[posPai:]#escreve os filhos na string do pai correspondente
 
     def posString(self,palavra,string):#retorna a posicao na string em que deve ser inserido os filhos do nó pai
@@ -55,22 +51,27 @@ class Arvore: #classe para exibir a arvore
         ts.rotation = 90#exibir árvore na vertical
         t.show(tree_style=ts)#mostra árvore
 
+def encontraParidade(matrix):#retorna a paridade da matrix(se é possível ou não resolve-lá)
+    aux = []
+    k=""
+    for i in matrix:#coloca tds numeros da matrix em uma string
+        for j in i:
+            if j!='':
+                k+=j
+    somatorioParidade = 0
+    for i in range(8):#8 é a quantidade de números no jogo dos oito
+        for j in range(i+1,8):
+            if(int(k[i])>int(k[j])):#se ouver números menores após o número que está em analise então soma mais 1
+                somatorioParidade+=1
+    return (somatorioParidade%2 == 0)
+        
+
 def encontraPosicao(matrix,value = ''):#retorna a posicao(linha,coluna) de um valor na matriz
     return [(index, row.index(value)) for index, row in enumerate(matrix) if value in row][0]
 
 def filhosPossiveis(matrix): #retorna todos os filhos possiveis do estado em branco
     indice = encontraPosicao(matrix) #retorna o indice do espaço em branco na matrix (linha,coluna)
     NosFilhos = [] #vetor que guarda todos filhos possiveis(matrizes)
-    if(indice[0]-1>=0):#tenta mover o espaço branco p linha anterior
-        a = deepcopy(matrix) #copia a matriz original para a variável w
-        a[indice[0]-1][indice[1]] = ''
-        a[indice[0]][indice[1]] = matrix[indice[0]-1][indice[1]]
-        NosFilhos.append(a)
-    if(indice[0]+1<=2):#tenta mover o espaço branco p linha sucessora
-        b = deepcopy(matrix)
-        b[indice[0]+1][indice[1]] = ''
-        b[indice[0]][indice[1]] = matrix[indice[0]+1][indice[1]]
-        NosFilhos.append(b)
     if(indice[1]-1>=0):#tenta mover o espaço branco p coluna anterior
         c = deepcopy(matrix)
         c[indice[0]][indice[1]-1] = ''
@@ -81,6 +82,16 @@ def filhosPossiveis(matrix): #retorna todos os filhos possiveis do estado em bra
         d[indice[0]][indice[1]+1] = ''
         d[indice[0]][indice[1]] = matrix[indice[0]][indice[1]+1]
         NosFilhos.append(d)
+    if(indice[0]-1>=0):#tenta mover o espaço branco p linha anterior
+        a = deepcopy(matrix) #copia a matriz original para a variável w
+        a[indice[0]-1][indice[1]] = ''
+        a[indice[0]][indice[1]] = matrix[indice[0]-1][indice[1]]
+        NosFilhos.append(a)
+    if(indice[0]+1<=2):#tenta mover o espaço branco p linha sucessora
+        b = deepcopy(matrix)
+        b[indice[0]+1][indice[1]] = ''
+        b[indice[0]][indice[1]] = matrix[indice[0]+1][indice[1]]
+        NosFilhos.append(b)
     return NosFilhos
     
 
@@ -106,30 +117,36 @@ def matrizNaTelaUpdate(mat):
     entrada9.insert(0,mat[2][2])
     time.sleep(0.01)#para o programa nessa linha por 1,5 segundos
 
-def somatorioMatriz(mat):#retorna o valor do somatorio da distancia relativa das peças para sua posição correta
-    matFinal = [['1','2','3'],['4','','5'],['6','7','8']]
+def somatorioMatriz(mat,matFinal=[['1','2','3'],['4','','5'],['6','7','8']]):#retorna o valor do somatorio da distancia relativa das peças para sua posição correta
     somatorio = 0
     for i in mat:
         for j in i:
-            posAtual = encontraPosicao(mat,j)
-            posCerta = encontraPosicao(matFinal,j)
-            distDaPosicaoCerta = abs(posAtual[0]-posCerta[0]) + abs(posAtual[1]-posCerta[1])#Distância de Manhattan
-            somatorio+=distDaPosicaoCerta
+            if not (j==''):#não considera a movimentação da pedra branca
+                posAtual = encontraPosicao(mat,j)
+                posCerta = encontraPosicao(matFinal,j)
+                distDaPosicaoCerta = abs(posAtual[0]-posCerta[0]) + abs(posAtual[1]-posCerta[1])#Distância de Manhattan
+                somatorio+=distDaPosicaoCerta
     return somatorio
 
-def menorSomatorio(filhos=[]):#vê qual filho tem o menor somatório
+def menorSomatorio(filhos=[]):#vê qual filho tem o menor somatório local(método guloso)
     distanciasDosFilhos = []
     for i in filhos:
         distanciasDosFilhos.append(somatorioMatriz(i))
     posVet = distanciasDosFilhos.index(min(distanciasDosFilhos))
     return filhos[posVet] #retorna o filho que tem a menor distancia das peças
-    
+
+
 def printaMatriz(matrix):#printa a matriz que foi passada
     for i in matrix:
         print(i)
     print()
 
 def buscaEmProfundidade(mat,threadKill=False):#Completo (não guarda estados visitados)
+    if not(encontraParidade(mat)): #verifica se a matriz não pode ser resolvida
+        messagebox.showwarning('Impossível','Impossível Resolver.\nTente com outra entrada!')
+        if(questao2):
+                pb.destroy()
+        return
     k="" #arvore em string
     nivel = 0 #guarda o nível atual da árvore
     arvore = Arvore() #Instancia a classe
@@ -142,6 +159,8 @@ def buscaEmProfundidade(mat,threadKill=False):#Completo (não guarda estados vis
         except:
             pass
         pai = noNaoVisitado.pop()#viu que matriz não era solução então tira da pilha
+        if(questao2):
+            matrizNaTelaUpdate(pai)
         if(np.array_equal(pai,[['1','2','3'],['4','','5'],['6','7','8']])): #Compara a matriz atual com a matriz de estado final
             messagebox.showinfo('Solução Encontrada',"A solução foi encontrada no nível: "+str(nivel)+" da árvore.")
             if(questao2):
@@ -151,16 +170,21 @@ def buscaEmProfundidade(mat,threadKill=False):#Completo (não guarda estados vis
             return
         nivel+=1
         vetPossiveis = filhosPossiveis(pai) #retorna os filhos possíveis do pai atual
-        random.shuffle(vetPossiveis)#embaralha os filhos
+        shuffle(vetPossiveis)#embaralha os filhos
         if(questao1):
             k=arvore.stringArvore(vetPossiveis,str(pai),k)#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)
         noNaoVisitado.append(vetPossiveis[0])#add nó filhos na pilha 
         if nivel==50 and not flag and questao1: #mostra a arvore quando tá no nível 50
             flag=True
             arvore.mostraArvore(k)
-        if(questao2):
-            matrizNaTelaUpdate(pai)
+
+
 def buscaEmLargura(mat,threadKill=False):#completo
+    if not(encontraParidade(mat)): #verifica se a matriz não pode ser resolvida
+        messagebox.showwarning('Impossível','Impossível Resolver.\nTente com outra entrada!')
+        if(questao2):
+                pb.destroy()
+        return
     arvore = Arvore() #Instancia a classe e já cria o nó raiz
     noNaoVisitado = [mat] #fila já começa com o nó raiz
     flag = False #exibe a árvore só uma vez
@@ -171,7 +195,9 @@ def buscaEmLargura(mat,threadKill=False):#completo
                 break
         except:
             pass
-        pai = noNaoVisitado[0]#primeiro da fila  
+        pai = noNaoVisitado[0]#primeiro da fila
+        if(questao2):
+            matrizNaTelaUpdate(pai)
         if(np.array_equal(pai,[['1','2','3'],['4','','5'],['6','7','8']])): #Compara a matriz atual com a matriz de estado final
             print("ACHOU A SOLUÇÃO")
             print("Quantidade na Fila:",len(noNaoVisitado))
@@ -185,19 +211,22 @@ def buscaEmLargura(mat,threadKill=False):#completo
         del(noNaoVisitado[0])#tira o nó pai da fila
         vetPossiveis = filhosPossiveis(pai) #retorna os filhos possíveis do nó pai
         if(questao1):
-            k=arvore.stringArvore(vetPossiveis,str(pai),k,'largura')#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)
+            k=arvore.stringArvore(vetPossiveis,str(pai),k)#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)
         for b in vetPossiveis:
             noNaoVisitado.append(b)#add todos nós filhos na fila
             if len(noNaoVisitado)==8 and not flag and questao1: #mostra a arvore quando tem 250 nós na pilha
                 flag=True
                 arvore.mostraArvore(k)
-        if(questao2):
-            matrizNaTelaUpdate(pai)
     return
 
 
 
 def buscaHeuristica(pai,threadKill=False):#completo #recebe o nó raiz primeiramente
+    if not(encontraParidade(pai)): #verifica se a matriz não pode ser resolvida
+        messagebox.showwarning('Impossível','Impossível Resolver.\nTente com outra entrada!')
+        if(questao2):
+                pb.destroy()
+        return
     arvore = Arvore() #Instancia a classe e já cria o nó raiz
     k=""
     visitados = [pai]
@@ -228,16 +257,77 @@ def buscaHeuristica(pai,threadKill=False):#completo #recebe o nó raiz primeiram
             messagebox.showwarning('Solução Inválida', 'Não foi encontrada uma solução para a matriz de entrada')
             return
         if(questao1):
-            k=arvore.stringArvore(vetPossiveis,str(pai),k,'heuristica')#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)
+            k=arvore.stringArvore(vetPossiveis,str(pai),k)#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)
         pai = menorSomatorio(vetPossiveis) #retorna os filho com as peças menos distantes da matriz correta e atribui como pai da vez
         if(questao2):
             matrizNaTelaUpdate(pai)
-def A(mat):#incompleto
-    return
+
+
+def nivelDoNo(pai=[[],[],[]],k=""):#retorna o nível do nó dada a string de árvore k
+    if k=="":
+        return 0 #nível 0
+    pai = str(pai).replace("], [","*").replace("[","").replace("]","").replace(",",' ')
+    posPai = k.find(pai)#econtra a posicao do pai na string
+    return k[posPai:].count(")") - k[posPai:].count("(") 
+    
+
+def A(pai,threadKill=False):#completo
+    if not(encontraParidade(pai)): #verifica se a matriz não pode ser resolvida
+        messagebox.showwarning('Impossível','Impossível Resolver.\nTente com outra entrada!')
+        if(questao2):
+                pb.destroy()
+        return
+    raiz = pai #raiz é a primeira matriz recebida
+    arvore = Arvore() #Instancia a classe e já cria o nó raiz
+    k=""
+    quantNos = 0#quantidade de nós expandidos
+    aberto = {}
+    fechado = {0 + somatorioMatriz(pai) : [pai]}#matriz : matriz,f(n) = g(n)+h(n) #guarda os nós já expandidos
+    while(True):
+        try:
+            if(threadKill.wait(1)):
+                break
+        except:
+            pass
+        #printaMatriz(pai)
+        if(np.array_equal(pai,[['1','2','3'],['4','','5'],['6','7','8']])): #Compara a matriz atual com a matriz de estado final
+            messagebox.showinfo('Solução Encontrada',"A solução foi encontrada!\nForam expandidos "+str(quantNos)+
+                                " nós!\nA profundidade da solução se encontra no nível "+str(g)+" da árvore.")
+            if(questao2):
+                pb.destroy()
+            elif(questao1):#exibe a árvore somente no quando acha a resposta
+                arvore.mostraArvore(k)
+            return
+        quantNos+=1
+        vetPossiveis = []#são os filhos possiveis, não repetidos, que podem colocar como arestas com seu nó pai
+        auxFilhos = filhosPossiveis(pai)
+        g = nivelDoNo(pai,k)+1
+        for filho in auxFilhos:
+            if( str(aberto.values()).find(str(filho))==-1 and str(fechado.values()).find(str(filho))==-1):#verifica se o filho não está em aberto e nem em fechado
+                    vetPossiveis.append(filho)
+                    f = g + somatorioMatriz(filho) #manhatham do filho para a raiz + manhatham do filho para o objetivo
+                    try:
+                        aberto[f].append(filho) # se for a primeira matriz para aquela chave(f(n))
+                    except:
+                        aberto[f] = [filho]
+        k=arvore.stringArvore(vetPossiveis[::-1],str(pai),k)#função que auxilia para criar a arvore (cria todas aresta possíveis com o nó pai)     
+        menorGlobal = min(aberto)
+        pai = aberto[menorGlobal][0] #pega os filhos que tem os menores valores globais e escolhe o primeiro filho, dentre estes, para expandir
+        del(aberto[menorGlobal][0])
+        if(aberto[menorGlobal]==[]): # se ficar vazio então excluí a key do dicionário
+            aberto.pop(menorGlobal,None)
+        try:
+            fechado[menorGlobal].append(pai) # se for a primeira matriz para aquela chave(f(n))
+        except:
+            fechado[menorGlobal] = [pai]
+        if(questao2):
+            matrizNaTelaUpdate(pai)
+        
 
 def start():
     global questao1,questao2,threadKill,thread
-    questao1 = messagebox.askyesno('Escolha','Deseja ver a árvore?')#pergunta se sim ou não
+    mensagemProcessando["text"] = "Processando..."
+    questao1 = messagebox.askyesno('Escolha','Deseja ver a árvore?\nObs: A árvore é exibida até certo nível!')#pergunta se sim ou não
     questao2 = False
     if not(questao1):
         questao2 = messagebox.askyesno('Escolha','Deseja ver a animação?')#pergunta se sim ou não
@@ -245,37 +335,53 @@ def start():
     print("Aguarde o resultado final!\n")
     if(str(escolha.get())=='profundidade'):
         if(questao2):
+            mensagemProcessando["text"] = ""
             threadKill = threading.Event()
             thread=Thread(target=buscaEmProfundidade,args=(matrix,threadKill))#passa os parametros e a função para a thread
             thread.start()
             loadingStatus()
             return
         buscaEmProfundidade(matrix)
+        mensagemProcessando["text"] = ""
     elif(str(escolha.get())=='largura'):
         if(questao2):
+            mensagemProcessando["text"] = ""
             threadKill = threading.Event()
             thread=Thread(target=buscaEmLargura,args=(matrix,threadKill))#passa os parametros e a função para a thread
             thread.start()
             loadingStatus()
             return
         buscaEmLargura(matrix)
+        mensagemProcessando["text"] = ""
     elif(str(escolha.get())=='heuristica'):
         if(questao2):
+            mensagemProcessando["text"] = ""
             threadKill = threading.Event()
             thread=Thread(target=buscaHeuristica,args=(matrix,threadKill))#passa os parametros e a função para a thread
             thread.start()
             loadingStatus()
             return
         buscaHeuristica(matrix)
+        mensagemProcessando["text"] = ""
     elif(str(escolha.get())=='A'):
+        if(questao2):
+            mensagemProcessando["text"] = ""
+            threadKill = threading.Event()
+            thread=Thread(target=A,args=(matrix,threadKill))#passa os parametros e a função para a thread
+            thread.start()
+            loadingStatus()
+            return
         A(matrix)
+        mensagemProcessando["text"] = ""
+    else:
+        messagebox.showinfo('Algoritmo não selecionado.',"Escolha um método de busca antes de iniciar!")
         
     
 
-###main####
-def telaHome():
+
+def telaHome():#reset
     apagaJanela()
-    global entrada,entrada1,entrada2,entrada3,entrada4,entrada5,entrada6,entrada7,entrada8,entrada9,escolha
+    global entrada,entrada1,entrada2,entrada3,entrada4,entrada5,entrada6,entrada7,entrada8,entrada9,escolha,mensagemProcessando
     mensagem = Label(window, text="Insira o estado Inicial", font="impact 20 normal")
     mensagem.pack()
     jogoDosOito = Frame(window)
@@ -300,21 +406,23 @@ def telaHome():
     entrada9.grid(row=2,column=2)
 
     #radioButtons:
-    mensagem = Label(window, text="Selecione um algoritmo de busca: ", font="arial 12 bold")
-    mensagem.pack()
+    mensagemAlg = Label(window, text="Selecione um algoritmo de busca: ", font="arial 12 bold")
+    mensagemAlg.pack()
     escolha = StringVar()#Guarda o valor escolhido
     escolha.set(1)
     escolha1 = ttk.Radiobutton(window,text='Busca em Profundidade', value='profundidade' ,cursor="hand2",variable = escolha)
     escolha2= ttk.Radiobutton(window,text='Busca em Largura', value='largura', cursor="hand2",variable = escolha)
-    escolha3 = ttk.Radiobutton(window,text='Busca Heurística - Alg Guloso', value='heuristica', cursor="hand2",variable = escolha)
+    escolha3 = ttk.Radiobutton(window,text='Busca Heurística - Alg. Guloso', value='heuristica', cursor="hand2",variable = escolha)
     escolha4 = ttk.Radiobutton(window,text='Busca A*', value='A', cursor="hand2",variable = escolha)
     escolha1.pack(padx=(0,0))
     escolha2.pack(padx=(0,33))
-    escolha3.pack(padx=(30,0))
+    escolha3.pack(padx=(33,0))
     escolha4.pack(padx=(0,79))
     botao = ttk.Button(window, text="Começar", command=start,cursor="hand2")
     botao.pack()
 
+    mensagemProcessando = Label(window, text="", font="Arial 10 normal")
+    mensagemProcessando.pack()
 def loadingStatus():
     frame = ttk.Frame()
     global pb
@@ -339,11 +447,13 @@ def apagaJanela():
     widget_list = all_children(window)
     for item in widget_list:
         item.pack_forget()
-        
+
+
+###main####        
 if __name__ == "__main__":  
     window = Tk()#instanciamos a classe tk
     window.title("IA - Jogo dos Oito")
-    window.geometry("300x300+200+100")#largura x altura + pos_x + posy
+    window.geometry("300x320+200+100")#largura x altura + pos_x + posy
     menu = Menu(window)
     window.config(menu=menu)
     menu.add_cascade(label='Resetar',command=telaHome)
